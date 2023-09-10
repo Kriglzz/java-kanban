f0d231bdc0fc96cce552810ff520758a33744bd4
Kriglz <Kriglz@yandex.ru>
2023-09-10 18:30:49 +0300

    feat: добавлена проверка работы с историей и set'ерами

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        TaskManager taskManager = new TaskManager();
+        InMemoryTaskManager inMemoryTaskManager = new InMemoryTaskManager();
+        InMemoryHistoryManager inMemoryHistoryManager = new InMemoryHistoryManager();
 
         Task task1 = new Task("задача", "описание");
         Task task2 = new Task("з2", "о2");
@@ -13,48 +14,61 @@ public class Main {
         Epic epic2 = new Epic("epic2", "descr2");
         Epic epic3 = new Epic("epic3", "descr3");
 
-        taskManager.addNewTask(task1);
-        taskManager.addNewTask(task2);
-        taskManager.addNewTask(task3);
+        inMemoryTaskManager.addNewTask(task1);
+        inMemoryHistoryManager.addToHistory(task1);
+        inMemoryTaskManager.addNewTask(task2);
+        inMemoryHistoryManager.addToHistory(task2);
+        inMemoryTaskManager.addNewTask(task3);
+        inMemoryHistoryManager.addToHistory(task3);
 
-        taskManager.addNewEpicTask(epic1);
+        inMemoryTaskManager.addNewEpicTask(epic1);
+        inMemoryHistoryManager.addToHistory(epic1);
 
         subTask1.setEpicId(epic1.getId());
-        taskManager.addNewSubTask(subTask1);
+        inMemoryTaskManager.addNewSubTask(subTask1);
+        inMemoryHistoryManager.addToHistory(subTask1);
         subTask2.setEpicId(epic1.getId());
-        taskManager.addNewSubTask(subTask2);
+        inMemoryTaskManager.addNewSubTask(subTask2);
+        inMemoryHistoryManager.addToHistory(subTask2);
 
-        taskManager.addNewEpicTask(epic2);
+        inMemoryTaskManager.addNewEpicTask(epic2);
 
         subTask3.setEpicId(epic2.getId());
-        taskManager.addNewSubTask(subTask3);
+        inMemoryTaskManager.addNewSubTask(subTask3);
+        inMemoryHistoryManager.addToHistory(subTask3);
 
-        taskManager.addNewEpicTask(epic3);
+        inMemoryTaskManager.addNewEpicTask(epic3);
+        inMemoryHistoryManager.addToHistory(epic3);
 
 
         System.out.println("Проверка работы Task");
         System.out.println(task1);
         System.out.println("========");
-        System.out.println(taskManager.getAllTask());
+        System.out.println(inMemoryTaskManager.getAllTask());
         System.out.println("========");
-        System.out.println(taskManager.getTask(2));
+        System.out.println(inMemoryTaskManager.getTask(2));
         System.out.println("========");
-        taskManager.deleteTask(3);
-        System.out.println(taskManager.getAllTask());
+        inMemoryTaskManager.deleteTask(3);
+        System.out.println(inMemoryTaskManager.getAllTask());
         System.out.println("========");
-        taskManager.deleteAllTask();
-        System.out.println(taskManager.getAllTask());
+        inMemoryTaskManager.deleteAllTask();
+        System.out.println(inMemoryTaskManager.getAllTask());
         System.out.println("Проверка работы Epic");
         System.out.println(epic1);
         System.out.println("========");
-        System.out.println(taskManager.getAllEpic());
+        System.out.println(inMemoryTaskManager.getAllEpic());
         System.out.println("========");
-        System.out.println(taskManager.getEpic(4));
+        System.out.println(inMemoryTaskManager.getEpic(4));
         System.out.println("========");
-        taskManager.deleteEpic(5);
-        System.out.println(taskManager.getAllEpic());
+        inMemoryTaskManager.deleteEpic(5);
+        System.out.println(inMemoryTaskManager.getAllEpic());
         System.out.println("========");
-        System.out.println(taskManager.getAllSubTask());
-        System.out.println(taskManager.getEpicSubTaskIds(4));
+        System.out.println(inMemoryTaskManager.getAllSubTask());
+        System.out.println(inMemoryTaskManager.getEpicSubTaskIds(4));
+        subTask1.setStatus(Status.IN_PROGRESS);
+        subTask1.setDescription("ЭТО ИЗМЕНЕННОЕ ОПИСАНИЕ");
+        subTask1.setName("ЭТО ИЗМЕНЕННОЕ НАЗВАНИЕ");
+        System.out.println(inMemoryHistoryManager.getHistory());
+
     }
 }
