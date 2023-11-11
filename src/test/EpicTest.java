1f98eed53f373ec1fa56dd8a3a2e1418dc47525e
Kriglz <Kriglz@yandex.ru>
2023-11-11 03:27:08 +0300

    фз-7

--- src/test/EpicTest.java
+++ src/test/EpicTest.java
@@ -35,12 +35,12 @@ class EpicTest {
     @Test
     public void shouldReturnNewWhenAllSubtasksNew(){
 
-        Epic epic = new Epic("epic", "description", LocalDateTime.of(2023, 11, 1, 1, 1), 1);
+        Epic epic = new Epic("epic", "description", LocalDateTime.of(2023, 11, 2, 1, 1), 1);
         taskManager.addNewEpicTask(epic);
         assertEquals(epic.getStatus(), Status.NEW);
-        SubTask subTask1 = new SubTask("subtask1", "description1", 1, LocalDateTime.of(2023, 11, 1, 1, 1), 1);
+        SubTask subTask1 = new SubTask("subtask1", "description1", 1, LocalDateTime.of(2023, 11, 3, 1, 1), 1);
         taskManager.addNewSubTask(subTask1);
-        SubTask subTask2 = new SubTask("subtask2", "description2", 1, LocalDateTime.of(2023, 11, 1, 1, 1), 1);
+        SubTask subTask2 = new SubTask("subtask2", "description2", 1, LocalDateTime.of(2023, 11, 4, 1, 1), 1);
         taskManager.addNewSubTask(subTask2);
         assertEquals(Status.NEW, epic.getStatus(), "Статус не совпадает");
         taskManager.deleteAllSubTask();
@@ -51,14 +51,14 @@ class EpicTest {
     @Test
     public void shouldReturnDoneWhenAllSubtasksDone(){
 
-        Epic epic = new Epic("epic", "description", LocalDateTime.of(2023, 11, 11, 1, 1), 1);
+        Epic epic = new Epic("epic", "description", LocalDateTime.of(2023, 11, 5, 1, 1), 1);
         epic.setId(1);
         taskManager.addNewEpicTask(epic);
-        SubTask subTask1 = new SubTask("subtask1", "description1", 1, LocalDateTime.of(2023, 11, 1, 1, 1), 1);
+        SubTask subTask1 = new SubTask("subtask1", "description1", 1, LocalDateTime.of(2023, 11, 6, 1, 1), 1);
         subTask1.setId(2);
         subTask1.setStatus(Status.DONE);
         taskManager.addNewSubTask(subTask1);
-        SubTask subTask2 = new SubTask("subtask2", "description2", 1, LocalDateTime.of(2023, 11, 1, 1, 1), 1);
+        SubTask subTask2 = new SubTask("subtask2", "description2", 1, LocalDateTime.of(2023, 11, 7, 1, 1), 1);
         subTask2.setId(3);
         subTask2.setStatus(Status.DONE);
         taskManager.addNewSubTask(subTask2);
@@ -70,14 +70,14 @@ class EpicTest {
     @Test
     public void shouldReturnInProgressWhenSubtasksDoneAndNew(){ // FAIL
 
-        Epic epic = new Epic("epic", "description", LocalDateTime.of(2023, 11, 1, 1, 1), 1);
+        Epic epic = new Epic("epic", "description", LocalDateTime.of(2023, 11, 8, 1, 1), 1);
         taskManager.addNewEpicTask(epic);
-        SubTask subTask1 = new SubTask("subtask1", "description1", 1, LocalDateTime.of(2023, 11, 1, 1, 1), 1);
+        SubTask subTask1 = new SubTask("subtask1", "description1", 1, LocalDateTime.of(2023, 11, 9, 1, 1), 1);
         subTask1.setId(2);
         taskManager.addNewSubTask(subTask1);
         subTask1.setStatus(Status.DONE);
         taskManager.updateSubTask(subTask1);
-        SubTask subTask2 = new SubTask("subtask2", "description2", 1, LocalDateTime.of(2023, 11, 1, 1, 1), 1);
+        SubTask subTask2 = new SubTask("subtask2", "description2", 1, LocalDateTime.of(2023, 11, 10, 1, 1), 1);
         subTask2.setId(3);
         taskManager.addNewSubTask(subTask2);
         taskManager.updateEpic(epic);
@@ -89,14 +89,14 @@ class EpicTest {
     @Test
     public void shouldReturnInProgressWhenAllSubtasksInProgress(){
 
-        Epic epic = new Epic("epic", "description", LocalDateTime.of(2023, 11, 1, 1, 1), 1);
+        Epic epic = new Epic("epic", "description", LocalDateTime.of(2023, 11, 11, 1, 1), 1);
         taskManager.addNewEpicTask(epic);
         assertEquals(epic.getStatus(), Status.NEW);
-        SubTask subTask1 = new SubTask("subtask1", "description1", 1, LocalDateTime.of(2023, 11, 1, 1, 1), 1);
+        SubTask subTask1 = new SubTask("subtask1", "description1", 1, LocalDateTime.of(2023, 11, 12, 1, 1), 1);
         subTask1.setId(2);
         subTask1.setStatus(Status.IN_PROGRESS);
         taskManager.addNewSubTask(subTask1);
-        SubTask subTask2 = new SubTask("subtask2", "description2", 1, LocalDateTime.of(2023, 11, 1, 1, 1), 1);
+        SubTask subTask2 = new SubTask("subtask2", "description2", 1, LocalDateTime.of(2023, 11, 13, 1, 1), 1);
         subTask2.setId(3);
         subTask2.setStatus(Status.IN_PROGRESS);
         taskManager.addNewSubTask(subTask2);
